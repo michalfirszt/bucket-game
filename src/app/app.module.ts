ecc6f5a6c7a6600b3390551b7730f86e45eb5697
michalfirszt <michalfiszu@gmail.com>
2019-12-02 18:29:04 +0100

    bucket-game-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BucketGameComponent } from './bucket-game/bucket-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BucketGameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/bucket-game/bucket-game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bucket-game',
+  templateUrl: './bucket-game.component.html',
+  styleUrls: ['./bucket-game.component.sass']
+})
+export class BucketGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
